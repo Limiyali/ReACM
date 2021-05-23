c8049a25270dcc05e4332d1e185b6d634bbd5fd9
lenosey <lenosey@foxmail.com>
2021-05-23 11:38:14 +0800

    5-22

--- multi_t.cpp
+++ multi_t.cpp
@@ -1,4 +1,4 @@
-#if 0
+#if 1
 #include <iostream>
 #include <thread>
 #include <mutex>
@@ -6,6 +6,8 @@
 #include <future>
 #include <atomic>
 #include <deque>
+#include <algorithm>
+#include <numeric>
 #include <condition_variable>
 using namespace std;
 mutex m;
@@ -96,9 +98,53 @@ void consumer() {
 	}
 }
 
+void f(int i= 2) {
+	cout << i << endl;
+}
+
+thread g() {
+	return thread(f,32);
+}
+
+template<typename Iterator,typename T>
+struct accumulate_block {
+	void operator()(Iterator first, Iterator end, T& res) {
+		res = accumulate(first, end, res);
+	}
+};
+
+template<typename T,typename Iterator>
+T parallel_accumulate(Iterator first, Iterator last, T init)
+{
+  const unsigned long len = distance(first, last);
+  if (!len) return init;
+  const unsigned long min_per_thread = 25;
+  const unsigned long max_threads = (len + min_per_thread - 1) / min_per_thread;
+  const unsigned long hardware_threads = std::thread::hardware_concurrency();
+  const unsigned long num_threads = min((hardware_threads != 0 ? hardware_threads : 2), max_threads);
+  const unsigned long block_size = len / num_threads; // 每个线程中的数据量
+  std::vector<T> res(num_threads);
+  std::vector<std::thread> threads(num_threads - 1); // 已有一个主线程，所以少一个线程
+  Iterator block_start = first;
+  for (unsigned long i = 0; i < num_threads - 1; ++i)
+  {
+    Iterator block_end = block_start;
+    std::advance(block_end, block_size); // block_end指向当前块的尾部
+    threads[i] = std::thread(accumulate_block<Iterator, T>{},
+      block_start, block_end, std::ref(res[i]));
+    block_start = block_end;
+  }
+  accumulate_block<Iterator, T>()(block_start, last, res[num_threads - 1]);
+  for_each(threads.begin(), threads.end(), std::mem_fn(&std::thread::join));
+  return accumulate(res.begin(), res.end(), init);
+}
+
 int main() {
-	cout << add(12, 22);
-	cout << "main finished\n";
+	vector<long long int>a(1000000);
+	for (long long int i = 0; i < 10000000000; i++)
+		a.push_back(i);
+	cout << accumulate<>(a.begin(), a.end(), (long long)0) << endl;
+	cout << parallel_accumulate(a.begin(), a.end(), (long long )0) << endl;
 	return 0;
 }
 
